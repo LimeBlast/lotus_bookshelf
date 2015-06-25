646419eca7abf7f45e32a3cad4f4c0ed2821c2f4
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-06-25 15:56:12 +0100

    added validation

--- apps/web/controllers/books/create.rb
+++ apps/web/controllers/books/create.rb
@@ -2,11 +2,20 @@ module Web::Controllers::Books
   class Create
     include Web::Action
 
-    def call(params)
-      book = Book.new(params[:book])
-      BookRepository.create(book)
+    params do
+      param :book do
+        param :title, presence: true
+        param :author, presence: true
+      end
+    end
 
-      redirect_to '/books'
+    def call(params)
+      if params.valid?
+        book = Book.new(params[:book])
+        BookRepository.create(book)
+
+        redirect_to '/books'
+      end
     end
   end
 end

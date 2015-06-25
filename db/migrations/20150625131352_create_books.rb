34c727c057cda3d60421a21d94bbbdcc3a7b0450
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-06-25 14:28:48 +0100

    had to remove the datetimes from the migration to continue the tutorial

--- db/migrations/20150625131352_create_books.rb
+++ db/migrations/20150625131352_create_books.rb
@@ -5,8 +5,6 @@ Lotus::Model.migration do
 
       column :title, String, null: false
       column :author, String, null: false
-      column :created_at, DateTime, null: false
-      column :updated_at, DateTime, null: false
     end
   end
 end

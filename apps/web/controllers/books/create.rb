1645e07c98ce90065a6e7219d9233d1e9cebb4b0
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-06-25 15:45:53 +0100

    I don't know the answer to this yet, so skipping until it's resolved https://github.com/lotus/lotus.github.io/pull/47

--- apps/web/controllers/books/create.rb
+++ apps/web/controllers/books/create.rb
@@ -3,6 +3,10 @@ module Web::Controllers::Books
     include Web::Action
 
     def call(params)
+      book = Book.new(params[:book])
+      BookRepository.create(book)
+
+      redirect_to '/books'
     end
   end
 end

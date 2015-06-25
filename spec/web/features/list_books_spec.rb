9a0b7ca1052b4c8785f7061c33832fe893fb0b6b
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-06-25 15:12:12 +0100

    another one down

--- spec/web/features/list_books_spec.rb
+++ spec/web/features/list_books_spec.rb
@@ -10,9 +10,6 @@ describe 'List books' do
 
   it 'displays each book on the page' do
     visit '/books'
-
-    within '#books' do
-      assert page.has_css?('.book', count: 2), 'Expected to find 2 books'
-    end
+    assert page.has_css?('.book', count: 2), "Expected to find 2 books"
   end
 end

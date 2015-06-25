cd99aaa0d31acc85f2764aee1ccf6b9d4e5fb74d
Daniel Hollands <daniel.hollands@intercity-uk.com>
2015-06-25 13:56:17 +0100

    gave our book model a couple of attirbutes

--- spec/bookshelf/entities/book_spec.rb
+++ spec/bookshelf/entities/book_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Book do
-  # place your tests here
+  it 'can be initialised with title attribute' do
+    book = Book.new(title: 'Refactoring')
+    book.title.must_equal 'Refactoring'
+  end
+
+  it 'can be initialised with author attribute' do
+    book = Book.new(author: 'Dutch McLovin')
+    book.author.must_equal 'Dutch McLovin'
+  end
 end
